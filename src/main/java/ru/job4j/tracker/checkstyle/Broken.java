0613128896b878cc637db704822cb5323a42b61b
kristina <paskoveckristina3@gmail.com>
2024-01-14 20:19:29 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    private static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+    private String surname, name;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
