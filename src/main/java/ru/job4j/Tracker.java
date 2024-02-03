dd5d1a7070bd0b8fbaff8d7e1973b576d4e5d531
kristina <paskoveckristina3@gmail.com>
2024-02-03 23:28:54 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/Tracker.java
+++ src/main/java/ru/job4j/Tracker.java
@@ -14,25 +14,18 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String key) {
         Item[] result = new Item[size];
         int count = 0;
         for (int i = 0; i < size; i++) {
-           if (items[i].getName().equals(key)) {
-               result[count] = items[i];
-               count++;
-           }
+            if (items[i].getName().equals(key)) {
+                result[count] = items[i];
+                count++;
+            }
         }
         return Arrays.copyOf(result, count);
     }
@@ -43,10 +36,32 @@ public class Tracker {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
             if (item != null) {
-               result[size] = item;
-               size++;
+                result[size] = item;
+                size++;
             }
         }
         return Arrays.copyOf(result, size);
+    }
+
+    private int indexOf(int id) {
+        int result = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
