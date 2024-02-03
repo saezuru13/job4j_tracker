05a63f28e69410c13a218c0d135e51955d041373
kristina <paskoveckristina3@gmail.com>
2024-02-04 01:05:13 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/StartUI.java
+++ src/main/java/ru/job4j/StartUI.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
+        Item item1 = new Item();
         System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        System.out.println(item);
     }
 }
