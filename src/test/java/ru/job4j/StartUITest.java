ad9548b9977632ca96af155d3efb2d574da00031
kristina <paskoveckristina3@gmail.com>
2024-03-12 01:37:22 +0300

    9.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/StartUITest.java
+++ src/test/java/ru/job4j/StartUITest.java
@@ -5,6 +5,7 @@ import ru.job4j.tracker.Input;
 import ru.job4j.tracker.MockInput;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 class StartUITest {
 
     @Test
@@ -27,5 +28,33 @@ class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("New Item");
         assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.replaceItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId())
+        };
+        StartUI.deleteItem(new MockInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted).isEqualTo(null);
+
     }
 }
