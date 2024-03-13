f6d4c948b50e44910680d890fb2980b6cdcc5110
kristina <paskoveckristina3@gmail.com>
2024-03-13 20:00:32 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+import ru.job4j.Item;
+import ru.job4j.Tracker;
+
+public class CreateAction implements UserAction {
+    @Override
+    public String name() {
+        return "Добавить новую заявку";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Создание новой заявки ===");
+        String name = input.askStr("Введите имя: ");
+        Item item = new Item(name);
+        tracker.add(item);
+        System.out.println("Добавленная заявка: " + item);
+        return true;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/FindByIdAction.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import ru.job4j.Item;
+import ru.job4j.Tracker;
+
+public class FindByIdAction implements UserAction {
+    @Override
+    public String name() {
+        return "Показать заявку по id";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Вывод заявки по id ===");
+        int id = input.askInt("Введите id: ");
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.println("Заявка с введенным id: " + id + " не найдена.");
+        }
+        return true;
+    }
+}
