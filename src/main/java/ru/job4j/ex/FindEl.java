2baa110d2ab12ba2f50ccb9d6e76e49d9aa53287
kristina <paskoveckristina3@gmail.com>
2024-04-23 15:34:51 +0300

    0.4. Пользовательские исключения. [#219367 #488611]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,12 +4,13 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
         for (int i = 0; i < value.length; i++) {
-           if (key.equals(value[i])) {
-               result = i;
-               break;
-           } else  {
-               throw new ElementNotFoundException("Element not found");
-           }
+            if (key.equals(value[i])) {
+                result = i;
+                break;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element not found");
         }
         return result;
     }
