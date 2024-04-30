d97d304fadcc34062dc7a2a0ab7df22b3c27b852
kristina <paskoveckristina3@gmail.com>
2024-04-30 23:51:32 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,44 @@
+package ru.job4j.tracker;
+
+import ru.job4j.Item;
+import ru.job4j.Tracker;
+
+public final class SingleTracker {
+    public static SingleTracker instance = null;
+
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
